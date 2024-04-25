cdaeeac135375e7becb67ba348a39320544a1095
BigWang <2972437973@qq.com>
2024-04-25 18:02:36 +0800

    feat: 添加 papper 组件，调整路由结构

--- fe/app/(translate)/index.tsx
+++ fe/app/(translate)/index.tsx
@@ -1,14 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
-import "../global.css";
 
 export default function Index() {
   return (
-    <View className="flex justify-center ml-8">
+    <View className="flex justify-center ml-8 mt-[100px]">
       <Text className='text-red-400'>Open up App.tsx to start working on your app!</Text>
       <Text>Hello World!!!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
