f8c7c0433479cfb02f5212d6361058f9f9659750
BigWang <2972437973@qq.com>
2024-05-16 20:12:01 +0800

    feat: 增加了语言选择器的外部样式

--- fe/components/translate/LangPickerModal/LangPickerModal.tsx
+++ fe/components/translate/LangPickerModal/LangPickerModal.tsx
@@ -1,4 +1,5 @@
-import { ScrollView, StyleSheet, Text } from "react-native";
+import { radiusBase } from "@/styles/base";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import Modal from "react-native-modal";
 
 interface LangPickerModalProps {
@@ -7,10 +8,16 @@ interface LangPickerModalProps {
 
 export default function LangPickerModal({ ...props }: LangPickerModalProps) {
   return (
-    <Modal {...props} backdropOpacity={0.3} className="flex w-full" style={styles.modal}>
-      <ScrollView className="bg-white w-full h-full">
-        <Text>I am the modal content!</Text>
-      </ScrollView>
+    <Modal
+      {...props}
+      backdropOpacity={0.3}
+      className="flex-1 w-full"
+      style={styles.modal}>
+      <View className="bg-white h-3/5 absolute bottom-0 w-full" style={[styles.radius]}>
+        <ScrollView className="w-full flex-1" style={styles.radius} contentContainerStyle={styles.contentContainer} showsVerticalScrollIndicator={false}>
+          <Text className="text-9xl">I am the modal content! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore, autem earum! At vero similique animi! Eum error optio, mollitia natus quas placeat deleniti a autem delectus sunt blanditiis odio voluptate.</Text>
+        </ScrollView>
+      </View>
     </Modal>
   );
 }
@@ -19,4 +26,12 @@ const styles = StyleSheet.create({
   modal: {
     margin: 0,
   },
+  radius: {
+    borderTopLeftRadius: radiusBase,
+    borderTopRightRadius: radiusBase,
+  },
+  contentContainer: {
+    paddingVertical: 40,
+    paddingHorizontal: 20,
+  }
 });
---
+++ fe/styles/base.ts
@@ -0,0 +1,1 @@
+export const radiusBase = 30;
